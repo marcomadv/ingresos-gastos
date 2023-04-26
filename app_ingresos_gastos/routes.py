aac2d518615a6e24af3db1fcbeb44ddaf194107b
marcomadv <imarcoma@gmail.com>
2023-04-26 12:59:19 +0200

    ultimos cambios

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -3,4 +3,19 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    datos=[
+        {'fecha':'01/02/2023',
+        'concepto':'Ropa',
+        'monto':'-150'},
+        {'fecha':'01/03/2023',
+        'concepto':'Salario',
+        'monto':'1500'},
+        {'fecha':'15/03/2023',
+        'concepto':'Supermercado',
+        'monto':'-200'}
+        ]
+    return render_template("index.html",data = datos)
+
+@app.route("/new")
+def create():
+    return render_template("new.html")
