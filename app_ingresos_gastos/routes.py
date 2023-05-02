dbc1c2359cb66abdaf58571dcc2623a4f3b71e6a
marcomadv <imarcoma@gmail.com>
2023-05-02 23:05:55 +0200

    mostrar errores en vista html, mantener datos formulario en caso error

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -1,24 +1,41 @@
 from app_ingresos_gastos import app
-from flask import render_template
+from flask import render_template,request, redirect
+import csv
+from datetime import date
 
 @app.route("/")
 def index():
-    datos=[
-        {'fecha':'01/02/2023',
-        'concepto':'Ropa',
-        'monto':'-150'},
-        {'fecha':'01/03/2023',
-        'concepto':'Salario',
-        'monto':'1500'},
-        {'fecha':'15/03/2023',
-        'concepto':'Supermercado',
-        'monto':'-200'}
-        ]
-    return render_template("index.html",data = datos, title="Lista")
+    datos=[]
+    #llamada al archivo
+    fichero = open('data/movimientos.csv','r')
+    #accediendo a cada registro del archivo y lo formatea
+    csvReader = csv.reader(fichero,delimiter=",",quotechar='"')
+    #recorrer el objeto csvReader y cargar cada registro en la lista datos
+    for items in csvReader:
+        datos.append(items)
 
-@app.route("/new")
+    return render_template("index.html",data=datos,title="Lista")
+
+@app.route("/new", methods=["GET","POST"])
 def create():
-    return render_template("new.html", title="Registro",tipoAccion = "Registro", tipoBoton= "Guardar")
+    if request.method == "POST":
+
+        errores = validateForm(request.form)
+        if errores:
+            return render_template("new.html", title="Registro",tipoAccion = "Registro", tipoBoton= "Guardar", error = errores,dataForm = request.form)
+
+        else:
+            #acceder al archivo y configurar para cargar un nuevo registro
+            mifichero = open ('data/movimientos.csv','a')
+            #llamar al metodo writer y configuramos el formato 
+            lectura = csv.writer(mifichero,delimiter=',',quotechar='"')
+            #registramos los datos recibidos 
+            lectura.writerow([request.form['fecha'],request.form['concepto'],request.form['monto']])
+            mifichero.close()
+        return redirect ("/") 
+    
+    else:
+        return render_template("new.html",title = "Registro", tipoBoton= "Guardar",dataForm = {})
 
 @app.route("/update")
 def edite():
@@ -27,3 +44,20 @@ def edite():
 @app.route("/delete")
 def remove():
     return render_template("delete.html", title="Eliminar")
+
+def validateForm(datosFormulario):
+    errores = [] #crear lista para guardar errores
+    hoy = date.today().isoformat() #capturo la fecha de hoy 
+    if datosFormulario['fecha'] > hoy:
+        errores.append("La fecha no puede ser mayor a la actual")
+    if datosFormulario['concepto'] == "":
+        errores.append("El concepto no puede estar vacio")
+    if float(datosFormulario['monto']) <= 0.0 or datosFormulario['monto'] == '':
+        errores.append("El monto debe ser mayor a 0 o distinto de vacio")
+    
+    return errores
+
+        
+    
+
+
---
+++ ejemplos.py
@@ -0,0 +1,30 @@
+# lectura de archivos
+'''
+with open('data/movimientos.csv',"r") as resultado:
+    leer = resultado.read()
+'''
+#otro ejemplo (lo guarda como una lista)
+'''
+resultado = open('data/movimientos.csv','r')
+lectura = resultado.readlines()
+print(lectura)
+'''
+
+import csv
+'''
+midato = []
+mifichero = open('data/movimientos.csv','r')
+lectura = csv.reader(mifichero)
+
+for items in lectura:
+    print(items)
+    midato.append(items)
+
+print("mi lista: ",midato) #lista de listas
+'''
+'''
+mifichero = open('data/movimientos.csv','a')
+lectura = csv.writer(mifichero,delimiter=',',quotechar='"')
+lectura.writerow(['25/04/2023','compra de zapatillas','-100'])
+mifichero.close()
+'''
